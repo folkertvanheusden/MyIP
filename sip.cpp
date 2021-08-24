91dff47744c0e6d676fe6162c128f34683426e88
folkert van heusden <mail@vanheusden.com>
2021-08-24 19:13:48 +0200

    Working on SIP

--- sip.cpp
+++ sip.cpp
@@ -33,6 +33,8 @@ void sip::input(const any_addr & src_ip, int src_port, const any_addr & dst_ip, 
 
 	std::vector<std::string> *parts = split(lines->at(0), " ");
 
+	dolog(debug, "%s", std::string((const char *)pl.first, pl.second).c_str());
+
 	if (parts->size() == 3 && parts->at(0) == "OPTIONS" && parts->at(2) == "SIP/2.0") {
 		reply_to_OPTIONS(src_ip, src_port, dst_ip, dst_port, lines);
 	}
@@ -48,7 +50,7 @@ void sip::input(const any_addr & src_ip, int src_port, const any_addr & dst_ip, 
 	delete lines;
 }
 
-void create_response_headers(std::vector<std::string> *const target, const std::vector<std::string> *const source, const size_t c_size)
+void create_response_headers(std::vector<std::string> *const target, const std::vector<std::string> *const source, const size_t c_size, const any_addr & my_ip)
 {
 	target->push_back("SIP/2 200 OK");
 
@@ -58,14 +60,22 @@ void create_response_headers(std::vector<std::string> *const target, const std::
 	auto str_call_id = find_header(source, "Call-ID");
 	auto str_cseq = find_header(source, "CSeq");
 
-//	if (str_via.has_value())
-//		target->push_back("Via: " + str_via.value());
+	if (str_via.has_value())
+		target->push_back("Via: " + str_via.value());
 
 	// swap from/to
+	if (str_to.has_value()) {
+		target->push_back("From: " + str_to.value());
+
+		std::string::size_type lt = str_to.value().rfind('<');
+		std::string::size_type gt = str_to.value().rfind('>');
+
+		std::string contact = str_to.value().substr(lt, gt - lt + 1);
+
+		target->push_back(myformat("Contact: %s", contact.c_str()));
+	}
 	if (str_from.has_value())
 		target->push_back("To: " + str_from.value());
-	if (str_to.has_value())
-		target->push_back("From: " + str_to.value());
 
 	if (str_call_id.has_value())
 		target->push_back("Call-ID: " + str_call_id.value());
@@ -73,9 +83,19 @@ void create_response_headers(std::vector<std::string> *const target, const std::
 	if (str_cseq.has_value())
 		target->push_back("CSeq: " + str_cseq.value());
 
+	target->push_back(myformat("Server: %s", my_ip.to_str().c_str()));
+
 	target->push_back("Allow: INVITE, ASK, CANCEL, OPTIONS, BYE");
 
-	target->push_back("User-Agent: MyIP");
+	if (str_to.has_value()) {
+		std::string::size_type lt = str_to.value().rfind('<');
+		std::string::size_type gt = str_to.value().rfind('>');
+
+		std::string contact = str_to.value().substr(lt, gt - lt + 1);
+
+		target->push_back(myformat("Contact: %s", contact.c_str()));
+	}
+
 	target->push_back("Content-Type: application/sdp");
 	target->push_back(myformat("Content-Length: %zu", c_size));
 }
@@ -96,7 +116,7 @@ void sip::reply_to_OPTIONS(const any_addr & src_ip, const int src_port, const an
 	std::string content_out = merge(content, "\r\n");
 
 	std::vector<std::string> hout;
-	create_response_headers(&hout, headers, content_out.size());
+	create_response_headers(&hout, headers, content_out.size(), dst_ip);
 	std::string headers_out = merge(hout, "\r\n");
 
 	std::string out = headers_out + "\r\n" + content_out;
@@ -120,10 +140,12 @@ void sip::reply_to_INVITE(const any_addr & src_ip, const int src_port, const any
 	std::string content_out = merge(content, "\r\n");
 
 	std::vector<std::string> hout;
-	create_response_headers(&hout, headers, content_out.size());
+	create_response_headers(&hout, headers, content_out.size(), dst_ip);
 	std::string headers_out = merge(hout, "\r\n");
 
 	std::string out = headers_out + "\r\n" + content_out;
+
+	dolog(debug, "%s", out.c_str());
 
 	u->transmit_packet(src_ip, src_port, dst_ip, dst_port, (const uint8_t *)out.c_str(), out.size());
 }
