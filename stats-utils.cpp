f681a0bf4c3ceb174e310b80cf41cb63eec051e9
folkert van heusden <mail@vanheusden.com>
2021-08-24 12:01:53 +0200

    \"average\" counter

--- stats-utils.cpp
+++ stats-utils.cpp
@@ -10,18 +10,23 @@ std::string stats_to_json(const uint8_t *const p, const int size)
 	const uint8_t *const p_end = &p[size];
 	const uint8_t *cur_p = p;
 
-	while(cur_p < p_end && cur_p[8]) {
+	while(cur_p < p_end && cur_p[16]) {
 		uint64_t *cnt_p = (uint64_t *)cur_p;
+		uint64_t *cnt_p2 = (uint64_t *)(cur_p + 8);
 
 		if (out.empty())
 			out = "{ ";
 		else
 			out += ", ";
 
-		out += myformat("\"%s\" : ", &cur_p[8]);
-		out += myformat("%lu", *cnt_p);
+		out += myformat("\"%s\" : ", &cur_p[16]);
 
-		cur_p += 32;
+		if (*cnt_p2)
+			out += myformat("%f", *cnt_p / double(*cnt_p2));
+		else
+			out += myformat("%lu", *cnt_p);
+
+		cur_p += 40;
 	}
 
 	out += " }";
