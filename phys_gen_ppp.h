a52ff9e1e06a01b5f97d01d3b6649134303cbd44
folkert van heusden <mail@vanheusden.com>
2022-12-30 19:56:02 +0100

    split ppp into a gen and serial part

---
+++ phys_gen_ppp.h
@@ -0,0 +1,65 @@
+// (C) 2022-2022 by folkert van heusden <mail@vanheusden.com>, released under Apache License v2.0
+#pragma once
+
+#include <atomic>
+#include <string>
+#include <termios.h>
+#include <thread>
+
+#include "any_addr.h"
+#include "phys_slip.h"
+#include "network_layer.h"
+#include "stats.h"
+
+
+std::vector<uint8_t> unwrap_ppp_frame(const std::vector<uint8_t> & payload, const std::vector<uint8_t> & ACCM);
+
+class phys_gen_ppp : public phys
+{
+protected:
+	const any_addr my_mac;
+
+	bool protocol_compression { false };
+	bool ac_field_compression { false };
+	bool lcp_options_acked    { false };
+	bool ipcp_options_acked   { false };
+	bool ipv6cp_options_acked { false };
+
+	uint32_t magic { 0x1234abcd };
+
+	std::vector<uint8_t> ACCM_tx;
+	std::vector<uint8_t> ACCM_rx;
+
+	const any_addr opponent_address;
+
+	uint16_t fcstab[256] { 0 };
+
+	void handle_lcp(const std::vector<uint8_t> & data);
+	void handle_ccp(const std::vector<uint8_t> & data);
+	void handle_ipcp(const std::vector<uint8_t> & data);
+	void handle_ipv6cp(const std::vector<uint8_t> & data);
+
+	void send_Xcp(const uint8_t code, const uint8_t identifier, const uint16_t protocol, const std::vector<uint8_t> & data);
+	void send_rej(const uint16_t protocol, const uint8_t identifier, const std::vector<uint8_t> & options);
+	void send_ack(const uint16_t protocol, const uint8_t identifier, const std::vector<uint8_t> & options);
+	void send_nak(const uint16_t protocol, const uint8_t identifier, const std::vector<uint8_t> & options);
+
+	std::vector<uint8_t> wrap_in_ppp_frame(const std::vector<uint8_t> & payload, const uint16_t protocol, const std::vector<uint8_t> & ACCM, const bool not_ppp_meta);
+
+	void process_incoming_packet(std::vector<uint8_t> packet_buffer);
+
+	virtual bool transmit_low(const std::vector<uint8_t> & payload, const uint16_t protocol, const std::vector<uint8_t> & ACCM, const bool not_ppp_meta) = 0;
+
+public:
+	phys_gen_ppp(const size_t dev_index, stats *const s, const std::string & name, const any_addr & my_mac, const any_addr & opponent_address);
+	phys_gen_ppp(const phys_gen_ppp &) = delete;
+	virtual ~phys_gen_ppp();
+
+	void start() override;
+
+	bool transmit_packet(const any_addr & dest_mac, const any_addr & src_mac, const uint16_t ether_type, const uint8_t *payload, const size_t pl_size) override;
+
+	any_addr::addr_family get_phys_type() override { return any_addr::mac; }
+
+	virtual void operator()() override = 0;
+};
--- phys_ppp.h
+++ phys_ppp.h
@@ -8,57 +8,26 @@
 #include <thread>
 
 #include "any_addr.h"
-#include "phys_slip.h"
 #include "network_layer.h"
+#include "phys_gen_ppp.h"
 #include "stats.h"
 
 
-class phys_ppp : public phys
+class phys_ppp : public phys_gen_ppp
 {
 private:
-	const any_addr my_mac;
-	int   fd              { -1 };
-
-	bool emulate_modem_xp     { false };
-	bool protocol_compression { false };
-	bool ac_field_compression { false };
-	bool lcp_options_acked    { false };
-	bool ipcp_options_acked   { false };
-	bool ipv6cp_options_acked { false };
-
-	uint32_t magic { 0x1234abcd };
-
-	std::vector<uint8_t> ACCM_tx;
-	std::vector<uint8_t> ACCM_rx;
-
-	const any_addr opponent_address;
-
-	uint16_t fcstab[256] { 0 };
-
 	std::mutex send_lock;
 
-	void handle_lcp(const std::vector<uint8_t> & data);
-	void handle_ccp(const std::vector<uint8_t> & data);
-	void handle_ipcp(const std::vector<uint8_t> & data);
-	void handle_ipv6cp(const std::vector<uint8_t> & data);
+	bool       emulate_modem_xp { false };
+	int        fd               { -1    };
 
-	void send_Xcp(const uint8_t code, const uint8_t identifier, const uint16_t protocol, const std::vector<uint8_t> & data);
-	void send_rej(const uint16_t protocol, const uint8_t identifier, const std::vector<uint8_t> & options);
-	void send_ack(const uint16_t protocol, const uint8_t identifier, const std::vector<uint8_t> & options);
-	void send_nak(const uint16_t protocol, const uint8_t identifier, const std::vector<uint8_t> & options);
-
-	std::vector<uint8_t> wrap_in_ppp_frame(const std::vector<uint8_t> & payload, const uint16_t protocol, const std::vector<uint8_t> & ACCM, const bool not_ppp_meta);
+protected:
+	bool transmit_low(const std::vector<uint8_t> & payload, const uint16_t protocol, const std::vector<uint8_t> & ACCM, const bool not_ppp_meta) override;
 
 public:
 	phys_ppp(const size_t dev_index, stats *const s, const std::string & dev_name, const int bps, const any_addr & my_mac, const bool emulate_modem_xp, const any_addr & opponent_address);
 	phys_ppp(const phys_ppp &) = delete;
 	virtual ~phys_ppp();
-
-	void start() override;
-
-	bool transmit_packet(const any_addr & dest_mac, const any_addr & src_mac, const uint16_t ether_type, const uint8_t *payload, const size_t pl_size) override;
-
-	any_addr::addr_family get_phys_type() override { return any_addr::mac; }
 
 	void operator()() override;
 };
