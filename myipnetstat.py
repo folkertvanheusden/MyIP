ef4be9080c10148e3a5d46401f00a8bce710db0f
folkert van heusden <mail@vanheusden.com>
2023-01-08 10:05:51 +0100

    commandline handling

--- myipnetstat.py
+++ myipnetstat.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 
 import socket
+import sys
 
 class myipud:
     def __init__(self, location):
@@ -8,7 +9,12 @@ class myipud:
 
         self.s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
-        self.s.connect(location)
+        try:
+            self.s.connect(location)
+
+        except FileNotFoundError as e:
+            print(f'Socket cannot be opened ({e}), is MyIP running?')
+            sys.exit(0)
 
     def request(self, req):
         req_b = (req + '\n').encode('ascii')
@@ -34,10 +40,29 @@ class myipud:
 
             self.buffer += data.decode('ascii')
 
+def help():
+    print(' * sessions')
+    print(' * list-devices  list the physical devices, to be used with start/stop-pcap')
+    print(' * start-pcap x  start pcap recording on device x')
+    print(' * stop-pcap x   stop pcap recording on device x')
+
+if len(sys.argv) == 1:
+    help()
+    sys.exit(1)
+
 mi = myipud('/tmp/myipstats.sock')
 
-print(mi.request('sessions'))
+if sys.argv[1] == 'sessions':
+    print(mi.request('sessions'))
 
-print(mi.request('list-devices'))
+elif sys.argv[1] == 'list-devices':
+    print(mi.request('list-devices'))
 
-print(mi.request('start-pcap|tap-myip'))
+elif sys.argv[1] == 'start-pcap':
+    print(mi.request('start-pcap|%s' % sys.argv[2]))
+
+elif sys.argv[1] == 'stop-pcap':
+    print(mi.request('stop-pcap|%s' % sys.argv[2]))
+
+else:
+    help();
