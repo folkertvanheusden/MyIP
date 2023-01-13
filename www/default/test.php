d3effb3e2f46d023b121b557eee49058876ddcec
Folkert van Heusden <folkert@vizorium.soleus.nu>
2023-01-13 09:02:08 +0100

    send http request headers from browser to php-cgi-bin

--- www/default/test.php
+++ www/default/test.php
@@ -1,1 +1,9 @@
-<?php phpinfo(); ?>
+<?php
+if (isset($_COOKIE['visits']))
+	$value = (int)$_COOKIE['visits'] + 1;
+else
+	$value = 1;
+
+setcookie('visits', "$value");
+?>
+<pre><?php echo print_r(get_defined_vars(), true); ?></pre>
