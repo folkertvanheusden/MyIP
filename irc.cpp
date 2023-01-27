5ffc9df75edf69d2e362bd78909f719e944b19f7
folkert van heusden <mail@vanheusden.com>
2023-01-27 15:25:53 +0100

    NICK can be send multiple times

--- irc.cpp
+++ irc.cpp
@@ -46,9 +46,6 @@ void irc_deinit()
 {
 }
 
-// I.S.: Internet Session
-typedef enum { IS_wait_nick, IS_wait_user, IS_running, IS_disconnect } irc_state_t;
-
 void transmit_to_channel(const std::string & channel, const std::string & msg_line)
 {
 	for(auto & nick : nicknames) {
@@ -84,172 +81,161 @@ void send_user_for_channel(const std::string & channel)
 	}
 }
 
-static void process_line(session *const tcp_session, irc_state_t *const is, const std::string & line)
+static bool process_line(session *const tcp_session, bool *const seen_nick, bool *const seen_user, const std::string & line)
 {
 	DOLOG(ll_debug, "irc::process_line: |%s|\n", line.c_str());
 
 	if (line.empty())
-		return;
+		return true;
 
 	std::vector<std::string> parts = split(line, " ");
 
 	if (parts.size() == 0)
-		return;
+		return true;
 
 	irc_session_data *isd = dynamic_cast<irc_session_data *>(tcp_session->get_callback_private_data());
 
-	if (*is == IS_wait_nick) {
-		if (parts.at(0) == "NICK" && parts.size() == 2) {  // ignoring hop count
-			// nick must be unique
-			auto nick = str_tolower(parts.at(1));
+	if (parts.at(0) == "NICK" && parts.size() == 2) {  // ignoring hop count
+		// nick must be unique
+		auto nick = str_tolower(parts.at(1));
 
-			std::unique_lock<std::mutex> lck(nicknames_lock);
+		std::unique_lock<std::mutex> lck(nicknames_lock);
 
-			auto it   = nicknames.find(nick);
-			if (it == nicknames.end()) {
-				person p;
+		auto it   = nicknames.find(nick);
+		if (it == nicknames.end()) {
+			person p;
 
-				p.tcp_session = tcp_session;
+			p.tcp_session = tcp_session;
 
-				nicknames.insert({ nick, p });
+			nicknames.insert({ nick, p });
 
-				lck.unlock();
+			lck.unlock();
 
-				isd->nick        = nick;
+			isd->nick  = nick;
 
-				*is = IS_wait_user;
-			}
-			else {
-				lck.unlock();
-
-				std::string error = ": 433 * " + nick + " :Nickname is already in use.\r\n";
-
-				if (tcp_session->get_stream_target()->send_data(tcp_session, reinterpret_cast<const uint8_t *>(error.c_str()), error.size()) == false)
-					*is = IS_disconnect;
-			}
+			*seen_nick = true;
 		}
 		else {
-			std::string error = ": 421 * :Something is not right.\r\n";
+			lck.unlock();
+
+			std::string error = ": 433 * " + nick + " :Nickname is already in use.\r\n";
 
 			if (tcp_session->get_stream_target()->send_data(tcp_session, reinterpret_cast<const uint8_t *>(error.c_str()), error.size()) == false)
-				*is = IS_disconnect;
+				return false;
 		}
+
+		return true;
 	}
-	else if (*is == IS_wait_user) {
-		if (parts.at(0) == "USER" && parts.size() >= 5) {
-			std::unique_lock<std::mutex> lck(nicknames_lock);
 
-			auto it = nicknames.find(isd->nick);
+	if (parts.at(0) == "USER" && parts.size() >= 5) {
+		std::unique_lock<std::mutex> lck(nicknames_lock);
 
-			if (it == nicknames.end()) {
-				lck.unlock();
+		auto it = nicknames.find(isd->nick);
 
-				std::string error = ": 401 * :What is your nick?\r\n";
+		if (it == nicknames.end()) {
+			lck.unlock();
 
-				if (tcp_session->get_stream_target()->send_data(tcp_session, reinterpret_cast<const uint8_t *>(error.c_str()), error.size()) == false)
-					*is = IS_disconnect;
-			}
-			else {
-				auto rn_offset = line.find(":");
-				auto real_name = rn_offset == std::string::npos ? "" : line.substr(rn_offset + 1);
+			std::string error = ": 401 * :What is your nick?\r\n";
 
-				it->second.real_name = real_name;
-
-				lck.unlock();
-
-				isd->username  = parts.at(1);
-
-				*is = IS_running;
-
-				std::vector<std::string> welcome {
-					": 001 " + isd->nick + " :Welcome\r\n",
-					": 002 " + isd->nick + " :Your host runs MyIP\r\n",
-					": 003 " + isd->nick + " :003\r\n",
-					": 004 " + isd->nick + " \r\n",
-					": 005 " + isd->nick + " :\r\n",
-					": 005 " + isd->nick + " :\r\n",
-					": 251 " + isd->nick + " :\r\n",
-					": 252 " + isd->nick + " 0 :operator(s) online\r\n",
-					": 253 " + isd->nick + " 0 :unknown connections\r\n",
-					": 254 " + isd->nick + " 0 :channels formed\r\n",
-					": 255 " + isd->nick + " :I have 0 clients and 1 server\r\n",
-					": 265 " + isd->nick + " :Current local users: 0  Max: 0\r\n",
-					": 266 " + isd->nick + " :Current global users: 0  Max: 0\r\n",
-					": 375 " + isd->nick + " :message of the day\r\n",
-					": 372 " + isd->nick + " :\r\n",
-					": 376 " + isd->nick + " :End of message of the day.\r\n"
-				};
-
-				for(auto & line : welcome) {
-					if (tcp_session->get_stream_target()->send_data(tcp_session, reinterpret_cast<const uint8_t *>(line.c_str()), line.size()) == false) {
-						*is = IS_disconnect;
-						break;
-					}
-				}
-			}
+			if (tcp_session->get_stream_target()->send_data(tcp_session, reinterpret_cast<const uint8_t *>(error.c_str()), error.size()) == false)
+				return false;
 		}
-	}
-	else if (*is == IS_running) {
-		if (parts.at(0) == "JOIN" && parts.size() >= 2) {
-			auto channels = split(parts.at(1), ",");
-
-			std::unique_lock<std::mutex> lck(nicknames_lock);
-
-			auto it = nicknames.find(isd->nick);
-
-			for(auto & channel : channels) {
-				it->second.channels.insert(str_tolower(channel));
-
-				std::string join_line = ":" + isd->nick + "!" + isd->username + "@" + tcp_session->get_their_addr().to_str() + " JOIN " + channel + "\r\n";
-
-				transmit_to_channel(channel, join_line);
-
-				send_user_for_channel(channel);
-			}
-
-			// TODO: notify each user
-		}
-		else if (parts.at(0) == "PART" && parts.size() >= 2) {
-			auto channels = split(parts.at(1), ",");
-
-			std::unique_lock<std::mutex> lck(nicknames_lock);
-
-			auto it = nicknames.find(isd->nick);
-
-			for(auto & channel : channels) {
-				it->second.channels.erase(str_tolower(channel));
-
-				send_user_for_channel(channel);
-
-				std::string part_line = ":" + isd->nick + "!" + isd->username + "@" + tcp_session->get_their_addr().to_str() + " PART " + channel + "\r\n";
-
-				transmit_to_channel(channel, part_line);
-			}
-		}
-		else if ((parts.at(0) == "PRIVMSG" || parts.at(0) == "NOTICE") && parts.size() >= 2) {
-			auto target = str_tolower(parts.at(1));
-
-			std::unique_lock<std::mutex> lck(nicknames_lock);
-
-			std::string msg_line = ":" + isd->nick + "!" + isd->username + "@" + tcp_session->get_their_addr().to_str() + " " + line + "\r\n";
-
-			transmit_to_channel(target, msg_line);
-		}
-		// TODO
 		else {
-			std::string error = ": 421 * :Unknown command.\r\n";
+			auto rn_offset = line.find(":");
+			auto real_name = rn_offset == std::string::npos ? "" : line.substr(rn_offset + 1);
 
-			if (tcp_session->get_stream_target()->send_data(tcp_session, reinterpret_cast<const uint8_t *>(error.c_str()), error.size()) == false)
-				*is = IS_disconnect;
+			it->second.real_name = real_name;
+
+			lck.unlock();
+
+			isd->username = parts.at(1);
+
+			*seen_user    = true;
+
+			std::vector<std::string> welcome {
+				": 001 " + isd->nick + " :Welcome\r\n",
+				": 002 " + isd->nick + " :Your host runs MyIP\r\n",
+				": 003 " + isd->nick + " :003\r\n",
+				": 004 " + isd->nick + " \r\n",
+				": 005 " + isd->nick + " :\r\n",
+				": 005 " + isd->nick + " :\r\n",
+				": 251 " + isd->nick + " :\r\n",
+				": 252 " + isd->nick + " 0 :operator(s) online\r\n",
+				": 253 " + isd->nick + " 0 :unknown connections\r\n",
+				": 254 " + isd->nick + " 0 :channels formed\r\n",
+				": 255 " + isd->nick + " :I have 0 clients and 1 server\r\n",
+				": 265 " + isd->nick + " :Current local users: 0  Max: 0\r\n",
+				": 266 " + isd->nick + " :Current global users: 0  Max: 0\r\n",
+				": 375 " + isd->nick + " :message of the day\r\n",
+				": 372 " + isd->nick + " :\r\n",
+				": 376 " + isd->nick + " :End of message of the day.\r\n"
+			};
+
+			for(auto & line : welcome) {
+				if (tcp_session->get_stream_target()->send_data(tcp_session, reinterpret_cast<const uint8_t *>(line.c_str()), line.size()) == false)
+					return false;
+			}
 		}
-	}
-	else {
-		std::string error = ": 421 * :Internal error.\r\n";
 
-		tcp_session->get_stream_target()->send_data(tcp_session, reinterpret_cast<const uint8_t *>(error.c_str()), error.size());
-
-		*is = IS_disconnect;
+		return true;
 	}
+
+	if (*seen_user == false || *seen_nick == false)
+		return true;
+
+	if (parts.at(0) == "JOIN" && parts.size() >= 2) {
+		auto channels = split(parts.at(1), ",");
+
+		std::unique_lock<std::mutex> lck(nicknames_lock);
+
+		auto it = nicknames.find(isd->nick);
+
+		for(auto & channel : channels) {
+			it->second.channels.insert(str_tolower(channel));
+
+			std::string join_line = ":" + isd->nick + "!" + isd->username + "@" + tcp_session->get_their_addr().to_str() + " JOIN " + channel + "\r\n";
+
+			transmit_to_channel(channel, join_line);
+
+			send_user_for_channel(channel);
+		}
+
+		return true;
+	}
+
+	if (parts.at(0) == "PART" && parts.size() >= 2) {
+		auto channels = split(parts.at(1), ",");
+
+		std::unique_lock<std::mutex> lck(nicknames_lock);
+
+		auto it = nicknames.find(isd->nick);
+
+		for(auto & channel : channels) {
+			it->second.channels.erase(str_tolower(channel));
+
+			send_user_for_channel(channel);
+
+			std::string part_line = ":" + isd->nick + "!" + isd->username + "@" + tcp_session->get_their_addr().to_str() + " PART " + channel + "\r\n";
+
+			transmit_to_channel(channel, part_line);
+		}
+
+		return true;
+	}
+
+	if ((parts.at(0) == "PRIVMSG" || parts.at(0) == "NOTICE") && parts.size() >= 2) {
+		auto target = str_tolower(parts.at(1));
+
+		std::unique_lock<std::mutex> lck(nicknames_lock);
+
+		std::string msg_line = ":" + isd->nick + "!" + isd->username + "@" + tcp_session->get_their_addr().to_str() + " " + line + "\r\n";
+
+		transmit_to_channel(target, msg_line);
+
+		return true;
+	}
+
+	return true;
 }
 
 void irc_thread(session *const tcp_session)
@@ -258,19 +244,21 @@ void irc_thread(session *const tcp_session)
 
         irc_session_data *ts = dynamic_cast<irc_session_data *>(tcp_session->get_callback_private_data());
 
-	irc_state_t is = IS_wait_nick;
+	bool seen_nick = false;
+	bool seen_user = false;
 
-        for(;ts->terminate == false && is != IS_disconnect;) {
+        for(;ts->terminate == false;) {
 		std::unique_lock<std::mutex> lck(ts->r_lock);
 
 		std::size_t crlf = ts->input.find("\r\n");
 
 		if (crlf != std::string::npos) {
-			process_line(tcp_session, &is, ts->input.substr(0, crlf));
+			if (process_line(tcp_session, &seen_nick, &seen_user, ts->input.substr(0, crlf)) == false)
+				break;
 
 			ts->input.erase(0, crlf + 2);
 		}
-		else if (is != IS_disconnect) {
+		else {
 			ts->r_cond.wait_for(lck, 500ms);
 		}
 	}
