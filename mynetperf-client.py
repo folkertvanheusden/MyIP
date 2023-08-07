a8ab8d751ae37fc2f4482c9a96d6874f1bdfc4d9
folkert van heusden <mail@vanheusden.com>
2023-08-07 08:00:01 +0200

    stats

--- mynetperf-client.py
+++ mynetperf-client.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 
 import json
+import math
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,23 +21,53 @@ def recv_reply(s: socket) -> str:
 
     return reply
 
+min_speed = math.inf
+max_speed = -math.inf
+n = 0
+total = 0
+median = []
+
 work = bytearray(131072)
 
-while True:
-    bs = 512
+work_bs = 65536
 
-    command = { "mode": "receive", "block_size": bs }
-    s.send(json.dumps(command).encode('ascii'))
-    s.send('\n'.encode('ascii'))
+try:
+    while True:
+        bs = work_bs
 
-    if json.loads(recv_reply(s))['result'] != 'ok':
-        break
+        command = { "mode": "receive", "block_size": bs }
+        s.send(json.dumps(command).encode('ascii'))
+        s.send('\n'.encode('ascii'))
 
-    while bs > 0:
-        cur_bs = min(bs, len(work))
+        if json.loads(recv_reply(s))['result'] != 'ok':
+            break
 
-        s.send(work[0:cur_bs])
+        while bs > 0:
+            cur_bs = min(bs, len(work))
 
-        bs -= cur_bs
+            s.send(work[0:cur_bs])
 
-    print(recv_reply(s))
+            bs -= cur_bs
+
+        j = json.loads(recv_reply(s))
+
+        speed = work_bs * 1000000 / j['took'] / 1024
+
+        median.append(speed)
+
+        total += speed
+        n += 1
+
+        min_speed = min(min_speed, speed)
+        max_speed = max(max_speed, speed)
+
+        print(j, f"{math.floor(speed)} kB/s")
+
+except KeyboardInterrupt as ki:
+    pass
+
+median.sort()
+
+median_val = median[n // 2] if (n & 1) else (median[n // 2] + median[n // 2 + 1]) / 2
+
+print(f"{n} transfers, average: {total/n:.3f} kB/s, median: {median_val:.3f} kB/s, minimum: {min_speed:.0f} kB/s, maximum: {max_speed:.0f} kB/s")
