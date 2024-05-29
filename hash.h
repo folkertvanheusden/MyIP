d2ce80c17cb6ffb8803cd1c640fa9d471ee7cddb
folkert van heusden <mail@vanheusden.com>
2024-05-29 21:37:34 +0200

    md5bin

--- hash.h
+++ hash.h
@@ -4,6 +4,8 @@
 
 
 uint64_t MurmurHash64A(const void *const key, const int len, const uint64_t seed);
+
+void md5bin(const uint8_t *const in, const size_t len, uint8_t *const h_out);
 std::string md5hex(const std::string & in);
 
 uint32_t crc32(const uint8_t *const data, const size_t n_data, const uint32_t polynomial);
