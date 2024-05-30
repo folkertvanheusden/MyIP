7a2c6b2410966ab47e1f6f2f544c8907e46366af
folkert <folkert@vanheusden.com>
2024-05-30 12:51:33 +0200

    missing implementation for mac_resolver::get_phys_by_mac

--- mac_resolver.cpp
+++ mac_resolver.cpp
@@ -162,4 +162,5 @@ std::optional<std::pair<phys*, any_addr> > mac_resolver::get_mac(phys *const int
 
 std::optional<phys *> mac_resolver::get_phys_by_mac(const any_addr & mac)
 {
+	return query_mac_cache(mac);
 }
